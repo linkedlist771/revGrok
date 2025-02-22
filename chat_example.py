f6b3bc6fdd016906da86628d284acde3d49ff0f5
LLinkedlist <72634327+linkedlist771@users.noreply.github.com>
2025-02-22 22:10:57 +0800

    Feature: initial commit

---
+++ chat_example.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from src.revgrok import GrokClient
+
+
+async def main():
+    cookie = "Your cookie here" # put your cookie here
+    model = "grok-3"
+    prompt = "9.8 and 9.11 which is bigger?"
+    client = GrokClient(cookie=cookie)
+    async for response in client.chat(prompt=prompt, model=model, reasoning=False):
+        print(response, end="")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
